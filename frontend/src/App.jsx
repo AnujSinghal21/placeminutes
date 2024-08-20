b8008b7e3aa02f273c4daef376203b81cb188abd
= <singhalanuj7575@gmail.com>
2024-08-20 23:52:58 +0530

    Started frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Insights from './components/Insights';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [godMode, setgodMode] = useState(0);
+  
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>
+          <ul>
+            <li><Link to="/"></Link></li>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/insights">Insights</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          {/* {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home godMode/>} />
+          <Route path="/home" element={<Home godMode/>} />
+          <Route path="/Insights" element={<Insights />} /> 
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/Insights.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Insights() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Welcome to the Insight Page</h1>
+      <p>This is the insight page of your React application.</p>
+      <p>Feel free to explore the other pages using the navigation links.</p>
+    </div>
+  );
+}
+
+export default Insights;
